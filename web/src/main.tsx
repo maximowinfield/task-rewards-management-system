2f8bbbc1f38dbdc56579f8132fc137623a1cb832
Maximo Winfield <maximo.winfield.7@gmail.com>
2025-12-17 02:09:19 -0500

    Working on Github Pages Routing

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -3,12 +3,6 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
-const params = new URLSearchParams(window.location.search);
-const p = params.get("p");
-if (p) {
-  window.history.replaceState(null, "", p);
-}
-
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter basename="/Microsoft-Fullstack-Sample">
