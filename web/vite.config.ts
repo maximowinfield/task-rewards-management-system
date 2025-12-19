aa88a54d6ffc0dd6a8fdba00e8423974e5950974
Maximo Winfield <maximo.winfield.7@gmail.com>
2025-12-19 05:49:38 -0500

    Fixing vite.config.ts

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/", // ✅ single-domain (API serves SPA)
+  base: "/Microsoft-Fullstack-Sample/", // ✅ single-domain (API serves SPA)
 });
